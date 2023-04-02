c4294d7c4d7f8cce481cee3e0b6255cc5f51d097
nazar12314 <59284695+nazar12314@users.noreply.github.com>
2023-04-02 15:39:35 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/neuralib.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for neuralib.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/CMakeFiles/neuralib.dir/layers/Initializer.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/neuralib.dir/layers/Initializer.cpp.o: \
- /mnt/c/Study/AKS/semestr_project/DeepLearning-Library/layers/Initializer.cpp \
- /usr/include/stdc-predef.h \
- /mnt/c/Study/AKS/semestr_project/DeepLearning-Library/layers/Initializer.h
