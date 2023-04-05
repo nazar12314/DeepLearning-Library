5dd3d0e54d39af3b1518cbfd0e4e87be8af35bd8
nazar12314 <kononenkonazar47@gmail.com>
2023-04-05 11:59:03 +0300

    added mnist data and parser for mnist

--- main.cpp
+++ main.cpp
@@ -1,49 +1,28 @@
 #include <iostream>
-#include <utils/TensorHolder.h>
+#include "mnist/mnist_reader.hpp"
+#include "mnist/mnist_utils.hpp"
 #include "eigen3/unsupported/Eigen/CXX11/Tensor"
-#include "layers/Dense.h"
-#include "utils/Optimizer.h"
-#include "utils/Loss.h"
-#include <chrono>
-#include "layers/Dense.h"
-#include "utils/Initializer.h"
-#include "models/Model.h"
 
-using namespace Eigen;
+int main(int argc, char* argv[]) {
+    // MNIST_DATA_LOCATION set by MNIST cmake config
+    std::cout << "MNIST data directory: " << MNIST_DATA_LOCATION << std::endl;
 
-int main() {
-//    initializers::RandomNormal<double> ci (10);
-//    Model<double> ml;
-//
-//    Tensor<double, 2> ts (10, 1);
-//    ts.setRandom();
-//
-//    TensorHolder<double> th (ts);
-//
-//    ml.addLayer(new DenseLayer<double> (10, 5, "Dense 1", ci));
-//    ml.addLayer(new DenseLayer<double> (5, 15, "Dense 2", ci));
-//
-//    std::cout << ml.predict(th).template get<2>();
+    // Load MNIST data
+    mnist::MNIST_dataset<std::vector, std::vector<uint8_t>, uint8_t> dataset =
+            mnist::read_dataset<std::vector, std::vector, uint8_t, uint8_t>(MNIST_DATA_LOCATION);
 
-    Tensor<double, 2> ts1 (3, 1);
-    ts1.setRandom();
-    TensorHolder<double> th1 (ts1);
+    mnist::binarize_dataset(dataset);
 
-    Tensor<double, 2> ts2 (3, 1);
-    ts2.setRandom();
-    TensorHolder<double> th2 (ts2);
-//    std::cout<<ts1<<std::endl<<std::endl<<ts2<<std::endl<<std::endl;
-//    auto error = (ts1 - ts2).pow(2).mean();
-//    std::cout<<error;
-//    return 0;
-//
-//    Tensor<double, 2> differ = (ts1-ts2);
-//    Tensor<double, 2> error = differ*differ.constant(2.0f/differ.dimension(0));
-//    std::cout<<error;
-//    return 0;
-    Loss<double>* l = new loss_functions::MSE<double>();
-    std::cout<<ts1<<std::endl<<std::endl<<ts2<<std::endl<<std::endl;
-    std::cout<<l->get_error(th1, th2)<<std::endl;
-    std::cout<<l->get_error_der(th1, th2).template get<2>();
+    Eigen::TensorMap<Eigen::Tensor<uint8_t, 1>> labels(dataset.training_labels.data(), dataset.training_labels.size());
+
+    // Get dimensions of the images
+    const size_t num_images = dataset.training_images.size();
+    const size_t image_size = dataset.training_images[0].size();
+
+    // Create 3D tensor to hold all training images
+    Eigen::TensorMap<Eigen::Tensor<uint8_t, 2>> images(dataset.training_images[0].data(), num_images, image_size);
+
+    std::cout << images.dimensions() << std::endl;
+
     return 0;
 }
