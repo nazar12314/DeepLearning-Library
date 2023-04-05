44b1c85159ec5edc90a467a97edfab07367f77d4
nazar12314 <kononenkonazar47@gmail.com>
2023-04-05 12:30:31 +0300

    added mnist library

---
+++ include/utils/MnistDataset.h
@@ -0,0 +1,76 @@
+//
+// Created by Nazar Kononenko on 05.04.2023.
+//
+
+#ifndef NEURALIB_MNISTDATASET_H
+#define NEURALIB_MNISTDATASET_H
+
+#include "eigen3/unsupported/Eigen/CXX11/Tensor"
+#include "utils/TensorHolder.h"
+#include "mnist/mnist_reader.hpp"
+#include "mnist/mnist_utils.hpp"
+
+using Eigen::Tensor;
+
+class MnistDataset {
+    mnist::MNIST_dataset<std::vector, std::vector<uint8_t>, uint8_t> dataset =
+            mnist::read_dataset<std::vector, std::vector, uint8_t, uint8_t>(MNIST_DATA_LOCATION);
+public:
+    MnistDataset() {
+        mnist::binarize_dataset(dataset);
+    }
+
+    TensorHolder<uint8_t> get_training_images() {
+        const size_t num_images = dataset.training_images.size();
+        const size_t image_size = dataset.training_images[0].size();
+
+        Eigen::TensorMap<Eigen::Tensor<uint8_t, 2>> images(
+                dataset.training_images[0].data(),
+                num_images,
+                image_size
+                );
+
+        Tensor<uint8_t, 2> images_tensor = images;
+
+        return TensorHolder<uint8_t>(images_tensor);
+    }
+
+    TensorHolder<uint8_t> get_training_labels() {
+        Eigen::TensorMap<Eigen::Tensor<uint8_t, 1>> labels(
+                dataset.training_labels.data(),
+                dataset.training_labels.size()
+                );
+
+        Eigen::Tensor<uint8_t, 1> tensor_labels = labels;
+
+        return TensorHolder<uint8_t>(tensor_labels);
+    }
+
+    TensorHolder<uint8_t> get_test_images() {
+        const size_t num_images = dataset.test_images.size();
+        const size_t image_size = dataset.test_images[0].size();
+
+        Eigen::TensorMap<Eigen::Tensor<uint8_t, 2>> images(
+                dataset.test_images[0].data(),
+                num_images,
+                image_size
+        );
+
+        Tensor<uint8_t, 2> images_tensor = images;
+
+        return TensorHolder<uint8_t>(images_tensor);
+    }
+
+    TensorHolder<uint8_t> get_test_labels() {
+        Eigen::TensorMap<Eigen::Tensor<uint8_t, 1>> labels(
+                dataset.test_labels.data(),
+                dataset.test_labels.size()
+        );
+
+        Eigen::Tensor<uint8_t, 1> tensor_labels = labels;
+
+        return TensorHolder<uint8_t>(tensor_labels);
+    }
+};
+
+#endif //NEURALIB_MNISTDATASET_H
--- main.cpp
+++ main.cpp
@@ -2,27 +2,14 @@
 #include "mnist/mnist_reader.hpp"
 #include "mnist/mnist_utils.hpp"
 #include "eigen3/unsupported/Eigen/CXX11/Tensor"
+#include "utils/MnistDataset.h"
 
 int main(int argc, char* argv[]) {
-    // MNIST_DATA_LOCATION set by MNIST cmake config
-    std::cout << "MNIST data directory: " << MNIST_DATA_LOCATION << std::endl;
+    MnistDataset mnst;
 
-    // Load MNIST data
-    mnist::MNIST_dataset<std::vector, std::vector<uint8_t>, uint8_t> dataset =
-            mnist::read_dataset<std::vector, std::vector, uint8_t, uint8_t>(MNIST_DATA_LOCATION);
+    TensorHolder<uint8_t> training_labels = mnst.get_training_labels();
 
-    mnist::binarize_dataset(dataset);
-
-    Eigen::TensorMap<Eigen::Tensor<uint8_t, 1>> labels(dataset.training_labels.data(), dataset.training_labels.size());
-
-    // Get dimensions of the images
-    const size_t num_images = dataset.training_images.size();
-    const size_t image_size = dataset.training_images[0].size();
-
-    // Create 3D tensor to hold all training images
-    Eigen::TensorMap<Eigen::Tensor<uint8_t, 2>> images(dataset.training_images[0].data(), num_images, image_size);
-
-    std::cout << images.dimensions() << std::endl;
+    std::cout << training_labels.get<1>() << std::endl;
 
     return 0;
 }
