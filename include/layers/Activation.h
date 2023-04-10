257d056115d7c4e38bae4b3aef6633f8b964623e
nazar12314 <kononenkonazar47@gmail.com>
2023-04-10 15:56:36 +0300

    deleted tensorHolder and its dependencies

--- include/layers/Activation.h
+++ include/layers/Activation.h
@@ -8,155 +8,59 @@
 #include <exception>
 #include "eigen3/unsupported/Eigen/CXX11/Tensor"
 #include "Layer.h"
-#include "utils/TensorHolder.h"
 
 using Eigen::Tensor;
 
-template<class T>
-class Activation : public Layer<T> {
+template<class T, size_t Dim>
+class Activation : Layer<T, Dim> {
 public:
-    Activation(const std::string & name,
-               std::function<TensorHolder<T>(const TensorHolder<T> &, std::vector<T> &)> activation_,
-               std::function<TensorHolder<T>(const TensorHolder<T> &, std::vector<T> &)> activation_prime_)
-               : activation(activation_), activation_prime(activation_prime_),
-                 Layer<T>(name, false){};
+    Activation() : Layer<T, Dim>("", false) {}
 
-    void set_weights(const TensorHolder<T> &) override {
+    void set_weights(const Tensor<T, Dim> &) override {
         throw std::logic_error("Weights aren't implemented for Activation class");
     }
 
-    const TensorHolder<T> &get_weights() override {
+    const Tensor<T, Dim> &get_weights() override {
         throw std::logic_error("Weights aren't implemented for Activation class");
     }
 
-    virtual TensorHolder<T> forward(const TensorHolder<T> &inputs) = 0;
+    virtual Tensor<T, Dim> forward(const Tensor<T, Dim> &inputs) = 0;
 
-    virtual TensorHolder<T> backward(const TensorHolder<T> &out_gradient, Optimizer<T> &optimizer) = 0;
+    virtual Tensor<T, Dim> backward(const Tensor<T, Dim> &out_gradient, Optimizer<T> &optimizer) = 0;
 
 protected:
-    std::function<TensorHolder<T>(const TensorHolder<T> &, std::vector<T> &)> activation;
-    std::function<TensorHolder<T>(const TensorHolder<T> &, std::vector<T> &)> activation_prime;
+    virtual Tensor<T, Dim> activation(const Tensor<T, Dim> &input, const std::vector<T> &) = 0;
+
+    virtual Tensor<T, Dim> activation_prime(const Tensor<T, Dim> &input, const std::vector<T> &) = 0;
 };
 
 
 namespace activations {
-    // ReLU
-    template<class T>
-    TensorHolder<T> relu(const TensorHolder<T> &input, std::vector<T> &) {
-        constexpr size_t Dim = 2;
-        const Tensor<T, Dim> &input_tensor = input.template get<Dim>();
-        Tensor<T, Dim> output_tensor = input_tensor.cwiseMax(Tensor<T, Dim>(input_tensor.dimensions()).setZero());
-        return TensorHolder<T>(output_tensor);
-    }
 
-    template<class T>
-        TensorHolder<T> relu_prime(const TensorHolder<T> &input, std::vector<T> &) {
-        constexpr size_t Dim = 2;
-        const Tensor<T, Dim> &input_tensor = input.template get<Dim>();
-        auto relu_derivative = [](T x) { return x > static_cast<T>(0) ? static_cast<T>(1) : static_cast<T>(0); };
-        Tensor<T, Dim> output_tensor = input_tensor.unaryExpr(relu_derivative);
-        return TensorHolder<T>(output_tensor);
-    }
-
-
-    template<class T>
-    class ReLU : public Activation<T> {
-    private:
-            std::vector<T> params{};
+    template<class T, size_t Dim>
+    class ReLU : public Activation<T, Dim> {
     public:
-        explicit ReLU(const std::string & name) : Activation<T>(name, relu<T>, relu_prime<T>) {}
+        ReLU() : Activation<T, Dim>() {}
 
-        TensorHolder<T> forward(const TensorHolder<T> &inputs) override {
-            return this->activation(inputs, params);
+        Tensor<T, Dim> forward(const Tensor<T, Dim> &inputs) override {
+            return this->activation(inputs, std::vector<T>());
         }
 
-        TensorHolder<T> backward(const TensorHolder<T> &out_gradient, Optimizer<T> &) override {
-            return this->activation_prime(out_gradient, params);
+        Tensor<T, Dim> backward(const Tensor<T, Dim> &out_gradient, Optimizer<T> &optimizer) override {
+            return this->activation_prime(out_gradient, std::vector<T>());
         }
 
-    };
-    template<class T>
-    TensorHolder<T> sigmoid(const TensorHolder<T> &input, std::vector<T> &) {
-        constexpr size_t Dim = 2;
-        const Tensor<T, Dim> &input_tensor = input.template get<Dim>();
-        const Tensor<T, Dim> &minus_input_tensor = -input_tensor;
-        Tensor<T, Dim> output_tensor = input_tensor.constant(1) / (input_tensor.constant(1) + minus_input_tensor.exp());
-        return TensorHolder<T>(output_tensor);
-    }
-
-    template<class T>
-    TensorHolder<T> sigmoid_prime(const TensorHolder<T> &input, std::vector<T> &) {
-        constexpr size_t Dim = 2;
-
-        const Tensor<T, Dim> &input_tensor = input.template get<Dim>();
-        const Tensor<T, Dim> &minus_input_tensor = -input_tensor;
-        Tensor<T, Dim> output_tensor = input_tensor.constant(1) / (input_tensor.constant(1) + minus_input_tensor.exp());
-
-        Tensor<T, Dim> out = output_tensor * (output_tensor.constant(1) - output_tensor);
-        return TensorHolder<T>(out);
-    }
-
-    template<class T>
-    class Sigmoid : public Activation<T> {
-    private:
-        std::vector<T> params{};
-    public:
-        explicit Sigmoid(const std::string & name) : Activation<T>(name, sigmoid<T>, sigmoid_prime<T>) {}
-
-        TensorHolder<T> forward(const TensorHolder<T> &inputs) override {
-            return this->activation(inputs, params);
+        Tensor<T, Dim> activation(const Tensor<T, Dim> &input, const std::vector<T> &) override {
+            return input.cwiseMax(0.0);
         }
 
-        TensorHolder<T> backward(const TensorHolder<T> &out_gradient, Optimizer<T> & optimizer) override {
-            return this->activation_prime(out_gradient, params);
+        Tensor<T, Dim> activation_prime(const Tensor<T, Dim> &input, const std::vector<T> &) override {
+            auto relu_derivative = [](T x) { return x > static_cast<T>(0) ? static_cast<T>(1) : static_cast<T>(0); };
+            return input.unaryExpr(relu_derivative);
         }
 
     };
 
-    template<class T>
-    TensorHolder<T> softmax(const TensorHolder<T> &input, std::vector<T> &) {
-        constexpr size_t Dim = 2;
-
-        Tensor<T, Dim> input_tensor = input.template get<Dim>();
-        input_tensor = input_tensor.exp();
-        const Tensor<T, 0> &input_tensor_sum = input_tensor.sum();
-        input_tensor = input_tensor / input_tensor.constant(input_tensor_sum(0));
-        return TensorHolder<T>(input_tensor);
-    }
-
-    template<class T>
-    TensorHolder<T> softmax_prime(const TensorHolder<T> &input, std::vector<T> &) {
-        constexpr size_t Dim = 2;
-
-        const Tensor<T, Dim> &input_tensor = input.template get<Dim>();
-
-        Tensor<T, Dim> softmax_gradient(input_tensor.dimensions());
-
-        for (int i = 0; i < softmax_gradient.dimension(0); ++i) {
-            for (int j = 0; j < softmax_gradient.dimension(1); ++j) {
-                softmax_gradient(i, j) = input_tensor(i) * ((i == j) ? 1.0 : 0.0) - input_tensor(i) * input_tensor(j);
-            }
-        }
-
-        return TensorHolder<T>(softmax_gradient);
-    }
-
-    template<class T>
-    class Softmax : public Activation<T> {
-    private:
-        std::vector<T> params{};
-    public:
-        explicit Softmax(const std::string & name) : Activation<T>(name, softmax<T>, softmax_prime<T>) {}
-
-        TensorHolder<T> forward(const TensorHolder<T> &inputs) override {
-            return this->activation(inputs, params);
-        }
-
-        TensorHolder<T> backward(const TensorHolder<T> &out_gradient, Optimizer<T> & optimizer) override {
-            return this->activation_prime(out_gradient, params);
-        }
-
-    };
 }
 
 
--- include/layers/Layer.h
+++ include/layers/Layer.h
@@ -7,26 +7,25 @@
 
 #include <iostream>
 #include <string>
-#include "utils/TensorHolder.h"
 #include "eigen3/unsupported/Eigen/CXX11/Tensor"
 #include "utils/Optimizer.h"
 
 using Eigen::Tensor;
 
-template<class T>
+template<class T, size_t Dim>
 class Layer {
     std::string name;
     bool trainable;
 public:
     Layer(const std::string & name, bool trainable): name(name), trainable(trainable) {};
 
-    virtual TensorHolder<T> forward(const TensorHolder<T> & inputs) = 0;
+    virtual Tensor<T, Dim> forward(const Tensor<T, Dim> & inputs) = 0;
 
-    virtual TensorHolder<T> backward(const TensorHolder<T> & out_gradient, Optimizer<T>& optimizer) = 0;
+    virtual Tensor<T, Dim> backward(const Tensor<T, Dim> & out_gradient, Optimizer<T>& optimizer) = 0;
 
-    virtual void set_weights(const TensorHolder<T> & weights_) = 0;
+    virtual void set_weights(const Tensor<T, Dim> & weights_) = 0;
 
-    virtual const TensorHolder<T> &get_weights() = 0;
+    virtual const Tensor<T, Dim> &get_weights() = 0;
 
     virtual ~Layer() = default;
 };
