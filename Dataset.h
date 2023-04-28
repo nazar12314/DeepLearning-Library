e299236e2b110c77dedb44c776f1f2577aada282
nazar <xfghbdgh@gmail.com>
2023-04-28 23:56:36 +0300

    Added Dataset Class

--- Dataset.h
+++ Dataset.h
@@ -16,86 +16,132 @@
 
 using Eigen::Tensor;
 
-template <class T, size_t DimX, size_t DimY>
+template <typename T>
+std::vector<T> fileToVector(const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        throw std::logic_error("Cannot open file of labels");
+    }
+    std::vector<T> values;
+    // Read the file contents into a string
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    std::string content = buffer.str();
+
+    // Parse the string to obtain values of type T separated by spaces
+    std::stringstream stream(content);
+    T value;
+    while (stream >> value) {
+        values.push_back(value);
+    }
+    return values;
+}
+
+template <class T, size_t Dim>
+void vectorToVectorOfTensors(std::vector<Eigen::Tensor<T, Dim>>& result, std::vector<T>& data, size_t rows, size_t cols, size_t depth = 0) {
+
+    size_t numTensors = data.size() / (rows * cols);
+
+    if constexpr (Dim == 2) {
+        for (size_t i = 0; i < numTensors; ++i) {
+            Eigen::TensorMap<Eigen::Tensor<T, 2>> tensor(data.data() + i * rows * cols, rows, cols);
+            result.push_back(tensor);
+        }
+    } else {
+        for (size_t i = 0; i < numTensors; ++i) {
+            Eigen::TensorMap<Eigen::Tensor<T, 3>> tensor(data.data() + i * depth * rows * cols, depth, rows, cols);
+            result.push_back(tensor);
+        }
+    }
+}
+
+template<class T, size_t NumDimensions>
+Eigen::Tensor<T, NumDimensions> vectorOfTensorsToTensor(const std::vector<Eigen::Tensor<T, NumDimensions>>& tensors) {
+    // Determine the size of the resulting tensor
+    int size = tensors.size() * tensors[0].size();
+
+    // Create a new tensor with the appropriate size
+    Eigen::Tensor<T, NumDimensions> result(tensors[0].dimensions());
+    result.resize(size, Eigen::NoChange);
+
+    // Concatenate the data from all the tensors into the new tensor
+    for (int i = 0; i < tensors.size(); ++i) {
+        Eigen::array<int, NumDimensions> offsets;
+        offsets.fill(i * tensors[i].size());
+        result.slice(offsets, tensors[i].dimensions()) = tensors[i];
+    }
+
+    return result;
+}
+
+
+
+template<class TX, class TY, size_t DimX, size_t DimY>
 class Dataset {
 public:
-    Tensor<T, DimX> X_train, X_test;
-    Tensor<T, DimY> y_train, y_test;
+    std::vector<Tensor<TX, DimX>> X_train, X_test;
+    std::vector<Tensor<TY, DimY>> y_train, y_test;
+
+    std::vector<size_t> dimsX, dimsy;
 
 public:
+    Dataset(const std::vector<size_t> &dimsX, const std::vector<size_t> &dimsy) : dimsX(dimsX), dimsy(dimsy) {
+        this->dimsX.resize(DimX, 0);
+        this->dimsX.resize(DimY, 0);
+    };
 
-    Dataset() = default;
+    void read_from_files(const std::string &path_to_labels, const std::string &path_to_targets) {
 
-    Dataset(const std::string& path_to_file) {}
-
-//    bool load_from_file(const std::string& path_to_file) {
-//        // Open the CSV file
-//        std::ifstream input_file(path_to_file);
-//        if (!input_file.is_open()) {
-//            std::cerr << "Failed to open file: " << path_to_file << std::endl;
-//            return false;
-//        }
-//
-//        // Read the data from the CSV file using Boost tokenizer
-//        std::vector<std::vector<T>> data;
-//        std::string line;
-//        while (std::getline(input_file, line)) {
-//            boost::tokenizer<boost::escaped_list_separator<char>> tokenizer(line);
-//            std::vector<T> row;
-//            for (auto it = tokenizer.begin(); it != tokenizer.end(); ++it) {
-//                T value;
-//                try {
-//                    value = boost::lexical_cast<T>(*it);
-//                }
-//                catch (const boost::bad_lexical_cast&) {
-//                    std::cerr << "Invalid data value: " << *it << std::endl;
-//                    return false;
-//                }
-//                row.push_back(value);
-//            }
-//            data.push_back(row);
-//        }
-//
-//        // Convert the data to Eigen tensor format
-//        size_t num_rows = data.size();
-//        size_t num_cols = data[0].size();
-//        X_train.resize(num_rows, DimX);
-//        y_train.resize(num_rows, DimY);
-//        for (size_t i = 0; i < num_rows; ++i) {
-//            for (size_t j = 0; j < num_cols - DimY; ++j) {
-//                X_train(i, j) = data[i][j];
-//            }
-//            for (size_t j = num_cols - DimY; j < num_cols; ++j) {
-//                y_train(i, j - num_cols + DimY) = data[i][j];
-//            }
-//        }
-//
-//        // Close the input file and return success
-//        input_file.close();
-//        return true;
-//    }
-
-    // Split the data in the dataset into training and testing sets
-    void split_data(double train_ratio = 0.8) {
-        if (train_ratio <= 0.0 || train_ratio >= 1.0) {
-            throw std::invalid_argument("Invalid train ratio. It must be in the range (0, 1).");
+        auto labels = fileToVector<TX>(path_to_labels);
+        if constexpr (DimX == 2) {
+            vectorToVectorOfTensors<TX, 2>(X_train,labels, dimsX[0], dimsX[1], 0);
+        } else {
+             vectorToVectorOfTensors<TX, 3>(X_train, labels, dimsX[0], dimsX[1], dimsX[2]);
         }
 
-        // Shuffle the indices of the data points
-        std::vector<size_t> indices(X_train.dimension(0));
-        std::iota(indices.begin(), indices.end(), 0);
-        std::shuffle(indices.begin(), indices.end(), std::default_random_engine());
+        auto targets = fileToVector<TX>(path_to_targets);
+        if constexpr (DimY == 2) {
+            vectorToVectorOfTensors<TY, 2>(y_train,targets, dimsX[0], dimsX[1], 0);
+        } else {
+            vectorToVectorOfTensors<TY, 3>(y_train, targets, dimsX[0], dimsX[1], dimsX[2]);
+        }
 
-        // Compute the number of training and testing data points based on the given ratio
-        const auto num_train = static_cast<size_t>(X_train.dimension(0) * train_ratio);
-        const size_t num_test = X_train.dimension(0) - num_train;
-
-        // Split the data into training and testing sets
-        X_test = X_train.slice(num_train, 0);
-        y_test = y_train.slice(num_train, 0);
-        X_train = X_train.slice(0, 0, num_train);
-        y_train = y_train.slice(0, 0, num_train);
     }
+
+    // Splits the data in the dataset into training and testing sets
+    void split_data(double train_ratio = 0.8) {
+        // Step 1: Calculate the number of samples to put in the test set.
+        size_t num_samples = X_train.size();
+        size_t num_test_samples = static_cast<size_t>(num_samples * (1.0 - train_ratio));
+
+        // Step 2: Randomly shuffle the indices of the samples.
+        std::vector<size_t> indices(num_samples);
+        std::iota(indices.begin(), indices.end(), 0);
+        std::shuffle(indices.begin(), indices.end(), std::mt19937(std::random_device()()));
+
+        // Step 3: Split the shuffled indices into training and test indices.
+        std::vector<size_t> train_indices(indices.begin(), indices.begin() + (num_samples - num_test_samples));
+        std::vector<size_t> test_indices(indices.begin() + (num_samples - num_test_samples), indices.end());
+
+        // Step 4: Create X_test and y_test vectors by copying the corresponding samples from X_train and y_train using the test indices.
+        X_test.clear();
+        y_test.clear();
+        for (const auto& index : test_indices) {
+            X_test.push_back(X_train[index]);
+            y_test.push_back(y_train[index]);
+        }
+
+        // Step 5: Remove the test samples from X_train and y_train using the training indices.
+        std::vector<Tensor<TX, DimX>> new_X_train;
+        std::vector<Tensor<TY, DimY>> new_y_train;
+        for (const auto& index : train_indices) {
+            new_X_train.push_back(X_train[index]);
+            new_y_train.push_back(y_train[index]);
+        }
+        X_train = new_X_train;
+        y_train = new_y_train;
+    }
+
 };
 
 #endif //NEURALIB_DATASET_H
